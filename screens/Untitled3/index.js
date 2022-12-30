31ec397b11207e92004bbf4a16939d1a64de5599
Ravinder sales <ravinder.y@crowdbotics.com>
2022-12-30 18:06:55 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,3 +1,5 @@
+import { Switch } from "react-native";
+import { ActivityIndicator } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +10,46 @@ const Untitled3 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ActivityIndicator style={styles.KTQXAUnp}></ActivityIndicator><ActivityIndicator style={styles.tlcWhHFZ}></ActivityIndicator><Switch style={styles.fZnIRdiV} value="test"></Switch></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  VjGqrocm: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute"
+  },
+  VuLiIIVA: {
+    width: 50,
+    height: 50
+  },
+  KTQXAUnp: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    left: 40,
+    top: 78,
+    transform: "rotate(4deg)",
+    color: "#d04646",
+    backgroundColor: "#da3e3e"
+  },
+  tlcWhHFZ: {
+    width: 50,
+    height: 50
+  },
+  fZnIRdiV: {
+    width: 91,
+    height: 44,
+    position: "absolute",
+    left: 78,
+    top: 90,
+    transform: "rotate(10deg)"
   }
 });
 export default Untitled3;
