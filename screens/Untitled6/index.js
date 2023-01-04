c3f596fd0b159da7d206434962253ffe9bf0f7c7
Ravinder sales <ravinder.y@crowdbotics.com>
2023-01-04 11:11:13 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled6/index.js
@@ -0,0 +1,30 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled6 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><TextInput style={styles.CuftisOi} value="Testing test test"></TextInput></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  CuftisOi: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 227,
+    height: 69,
+    position: "absolute",
+    left: 32,
+    top: 60
+  }
+});
+export default Untitled6;
